cc2feb061bb327a296859cc414f0733e758c55c0
Issames <lamchabragissame.112@gmail.com>
2024-01-03 13:29:36 -0800

    Mendatory task

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def print_low_alpha():
-    for c in 'abcdefghijklmnopqrstuwxyz':
-        print('{}'.format(c), end = '')
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        print('{}'.format(c), end='')
 
 if __name__ == '__main__':
     print_low_alpha()
