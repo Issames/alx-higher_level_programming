6331ca96b43ee6d0047406dcf3cae8ec7b4ffbd2
Issames <lamchabragissame.112@gmail.com>
2024-01-03 13:53:53 -0800

    Mendatory

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,22 +3,22 @@ import random
 
 
 def get_randn():
-    num = random.randint(-10000, 10000)
-    return num
+    number = random.randint(-10000, 10000)
+    return number
 
 
-def get_last_digit(num):
-    if num >= 0:
-        return num % 10
+def get_last_digit(number):
+    if number >= 0:
+        return number % 10
     else:
-        if (num % 10) != 0:
-            return -10 + (num % 10)
+        if (number % 10) != 0:
+            return -10 + (number % 10)
         else:
             return 0
 
 
 def print_last_digit(last_digit):
-    print('Last digit of {:d} is {:d}'.format(num, last_digit), end=" ")
+    print('Last digit of {:d} is {:d}'.format(number, last_digit), end=" ")
     if last_digit > 5:
         print('{}'.format('and is greater than 5'))
     elif last_digit == 0:
@@ -28,6 +28,6 @@ def print_last_digit(last_digit):
 
 
 if __name__ == '__main__':
-    num = get_randn()
-    last_digit = get_last_digit(num)
+    number = get_randn()
+    last_digit = get_last_digit(number)
     print_last_digit(last_digit)
