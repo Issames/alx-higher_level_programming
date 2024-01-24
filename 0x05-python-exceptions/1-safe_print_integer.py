0a3fc2d77ef8aa5087d43f6e2325cc6aa7c72dcf
Issames <lamchabragissame.112@gmail.com>
2024-01-23 23:43:07 -0800

    Second Commit

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,8 +10,7 @@ def safe_print_integer(value):
     except TypeError:
         return False
 
-
-if __name__ = '__main__':
+if __name__ == '__main__':
     value = 89
     has_been_print = safe_print_integer(value)
     if not has_been_print:
